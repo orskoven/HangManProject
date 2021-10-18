4b451779a8203e2ad935f42a2951803322a11999
Simon Pedersen <90264457+orskoven@users.noreply.github.com>
2021-10-18 22:49:04 +0200

    some changes11

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,15 @@
-public class Game {
+import java.util.Scanner;
 
+public class Game {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome to hang-man!\nLet's play..");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Please type a character-guess:");
+            String inputCharGuessString = scanner.next();
+            char[] charArrayOfGuess = inputCharGuessString.toCharArray();
+            GameMode gameOne = new GameMode();
+            gameOne.matchCharacterWithString(charArrayOfGuess[i],"abe");
+        }
+    }
+    static Scanner scanner = new Scanner(System.in);
 }
--- src/GameMode.java
+++ src/GameMode.java
@@ -1,7 +1,35 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class GameMode {
-    private ArrayList<char[]> charGuess = new ArrayList<char[]>();
-    private ArrayList
+    private char charGuess;
+    private ArrayList<Character> charGuessArray = new ArrayList<Character>();
+    private String wordToBeGuessed;
+    private char[] alphabet = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
+    public GameMode(String wordToBeGuessed) {
+        this.wordToBeGuessed = wordToBeGuessed;
+    }
+    public void matchCharacterWithString(){
+        char[] charArrayOfGuess = new char[wordToBeGuessed.length()];
+        System.out.println("Hello and welcome to hang-man!\nLet's play..");
+        char[] wordToCharArray = wordToBeGuessed.toCharArray();
+        for (int i = 0; charGuessArray.size() == wordToBeGuessed.length();i++) {
+            System.out.println("Please type a character-guess:");
+            String inputCharGuessString = scanner.next();
+            charArrayOfGuess = inputCharGuessString.toCharArray();
+            if (inputCharGuessString == charArrayOfGuess[i]){
+                this.charGuessArray.set(i,charGuessing);
+                i++;
+            } else {
+                this.charGuessArray.set(i,'_');
+                i--;
+            }
+
+        }
+        System.out.println(charGuess);
+
+    }
+    static Scanner scanner = new Scanner(System.in);
 }
